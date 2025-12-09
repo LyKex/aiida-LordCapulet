c7b3cf57b28199b70b67d48b2dd21e3ad5f450a0
alberto-carta <carta.alberto@gmail.com>
2025-12-09 16:41:22 +0100

    Cleaned up extractor example

--- examples/example_extract_workchain.py
+++ examples/example_extract_workchain.py
@@ -22,39 +22,3 @@ data = extractor.extract_from_workchain(workchain_pk)
 
 # Save to JSON
 extractor.save_to_json(data, f"{material_name}_scan_data_extractor_redone.json")
-# extractor.save_to_json(data, f"{material_name}_scan_data_extractor.json")
-
-# Print summary
-#%%
-print(extractor.get_extraction_summary(data))
-
-#%% 
-#%%
-import numpy as np
-# Example: Analyze calculation 71782 and get regularization results
-extractor = WorkchainExtractor(perform_so_n=True, sanity_check_reconstruct_rho=True, debug=True)
-# calc_data = extractor.extract_single_calculation(71782)
-calc_data = extractor.extract_single_calculation(64802)
-
-# Get regularization summary and details
-print(extractor.get_regularization_summary())
-reg_details = extractor.get_regularization_details()
-print(f"Regularization details: {reg_details}")
-
-
-#manually check the data
-#%%
-occup_data = calc_data['output_atomic_occupations']['1']
-
-recon_data = calc_data['so_n_decomposition']['atom_decompositions']['1']
-
-down_matrix = np.array(recon_data['down_spin']['sanity_check']['reconstructed_density_matrix'])
-
-with np.printoptions(precision=3, suppress=True):
-    print("Original down spin occupation matrix:")
-    print(np.array(occup_data['spin_data']['down']['occupation_matrix']))
-    print("Reconstructed down spin occupation matrix:")
-    print(down_matrix)
-    print("Difference:")
-    print(np.array(occup_data['spin_data']['down']['occupation_matrix']) - down_matrix)
-#%%
